2dd1d3542cf180a6fa5786bccf129a443abd8551
lorenzo-ferrari <lorenzo.ferrari@lsgalilei.org>
2022-03-05 14:16:10 +0100

    added compressed Li Chao Tree

---
+++ Li Chao Tree/LCT.cpp
@@ -0,0 +1,59 @@
+#include <vector>
+#include <algorithm>
+#include <numeric>
+using namespace std;
+using LL = long long;
+
+#define all(x) x.begin(), x.end()
+
+struct fn {
+  LL m, q;
+  explicit fn(LL _m, LL _q) : m(_m), q(_q) {}
+  LL eval(LL x) const { return m*x + q; }
+};
+
+struct LCT_ZIP {
+  int n;
+  vector<fn> t;
+  vector<LL> qs;
+  vector<fn> history;
+  explicit LCT_ZIP(vector<LL> _qs) : qs(_qs) {
+    sort(all(qs));
+    qs.resize(unique(all(qs)) - qs.begin());
+    n = qs.size();
+    t.assign(4 * n, fn(0, 1e15));
+  }
+  explicit LCT_ZIP(int _n) {
+    vector<LL> q(_n);
+    iota(q.begin(), q.end(), 0);
+    *this = LCT_ZIP(q);
+  }
+  void add_fn(fn f) {
+    history.push_back(f);
+    if (!qs.empty()) add_fn(f, 1, 0, n);
+  }
+  void add_fn(fn f, int i, int l, int r) {
+    int m = (l + r) / 2;
+    bool lf = f.eval(qs[l]) < t[i].eval(qs[l]);
+    bool md = f.eval(qs[m]) < t[i].eval(qs[m]);
+    if (md) {
+      swap(t[i], f);
+    }
+    if (r - l <= 1) return;
+    if (lf != md) {
+      add_fn(f, 2*i, l, m);
+    } else {
+      add_fn(f, 2*i+1, m, r);
+    }
+  }
+  LL get(int x, int i = 1, int l = 0, int r = -1) {
+    if (r == -1) r = n;
+    int m = (l + r) / 2;
+    if (r - l <= 1) return t[i].eval(x);
+    if (x < qs[m]) {
+      return min(t[i].eval(x), get(x, 2*i, l, m));
+    } else {
+      return min(t[i].eval(x), get(x, 2*i+1, m, r));
+    }
+  }
+};
